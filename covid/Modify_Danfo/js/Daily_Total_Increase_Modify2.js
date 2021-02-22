40a7137d6450b9c387fa70582485d952ad9e0584
khw970421 <khw970421@kakao.com>
2021-02-22 19:46:07 +0900

    2/22 버튼추가 및 bar/line 차트 추가

--- covid/Modify_Danfo/js/Daily_Total_Increase_Modify2.js
+++ covid/Modify_Danfo/js/Daily_Total_Increase_Modify2.js
@@ -1,4 +1,49 @@
 import {url} from './Data.js'
+
+const lineButton = document.getElementById('lineButton')
+const barButton = document.getElementById('barButton')
+const cancelLine = document.getElementById('cancelLine')
+const cancelBar = document.getElementById('cancelBar')
+
+const startLineChart = function(){
+  load().then((value)=>{value.plot('line').line();})
+}
+const startBarChart = function(){
+  load().then((value)=>{value.plot('bar').bar();})
+}
+
+const cancelLineChart = function(){
+  if(document.getElementById('line').childNodes[0]==undefined){
+    alert('삭제할 내용이 없어');
+    return 0;
+  }
+
+  const chart = confirm("원 차트를 지우겠습니까?");
+  if (chart == false)
+    alert('지우기 취소')
+  else
+    document.querySelector('#line > .plot-container').remove();
+}
+
+const cancelBarChart = function(){
+  if(document.getElementById('bar').childNodes[0]==undefined){
+    alert('삭제할 내용이 없어');
+    return 0;
+  }
+
+  const chart = confirm("원 차트를 지우겠습니까?");
+  if (chart == false)
+    alert('지우기 취소')
+  else
+    document.querySelector('#bar > .plot-container').remove();
+}
+
+
+lineButton.addEventListener('click',startLineChart);
+barButton.addEventListener('click',startBarChart);
+cancelLine.addEventListener('click',cancelLineChart);
+cancelBar.addEventListener('click',cancelBarChart);
+
 async function load () {
   const get_date = [];
   const get_sum = [];
@@ -11,16 +56,16 @@ async function load () {
     get_date.push(data.body__items__item__createDt.data[0].slice(2,10));
   })
 
-  let df_sum1 = new dfd.DataFrame({'sum':get_sum},{index:get_date});  //df_sum은 Series 형태이므로 DataFrame 형태로 변환
-  df_sum1.plot('plot').line();
+  let df_sum = new dfd.DataFrame({'sum':get_sum},{index:get_date});  //df_sum은 Series 형태이므로 DataFrame 형태로 변환
+  return df_sum;
+  //df_sum.plot('bar').bar();
 }
-load();
 
 function Set_Date(){
   let tDate = new Date('2020-03-03'); // 2020년 03월 04일 부터 시작
   let Year,Month,Day;                 // 각 날짜별 날짜 생성
   const date_array = [];              // 해당 필요부분 넣을 배열 생성
-  for(let i=0;i<58;i++)
+  for(let i=0;i<89;i++)
   {
     tDate.setDate(tDate.getDate()+1)        // 3월 4일 계산 후 하루씩 증가
     Year = tDate.getFullYear().toString().slice(2,4);       // 2020년이 아닌 뒤의 두자리 수만 필요하므로 slice 사용
@@ -30,3 +75,6 @@ function Set_Date(){
   }
   return date_array;     //해당 배열을 반환한다.
 }
+
+
+
