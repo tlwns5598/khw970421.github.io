f4d6ab3003a720cb35b052d6346a118193fcea8b
khw970421 <khw970421@kakao.com>
2021-03-14 16:00:43 +0900

    Compare_City2.js  코드 간략 수정

--- covid/Modify_Danfo/js/Compare_City2.js
+++ covid/Modify_Danfo/js/Compare_City2.js
@@ -1,25 +1,23 @@
 import {url} from './Data.js'
-const input = document.getElementById('city')
+const cityTag = document.getElementById('city')
 const addButton = document.getElementById('addButton');
-const city = document.querySelector('.cityMember');
+const cityTitle = document.querySelector('.cityMember');
 let button = document.getElementById('but');
 
 let cityMember=[];
 
 const addEventListener1 = ()=>{
-  city.insertAdjacentHTML('beforeend',`<button type="button" class="m-1 btn btn-primary">${input.value}</button>`);
-  cityMember.push(input.value)
+  cityTitle.insertAdjacentHTML('beforeend',`<button type="button" class="m-1 btn btn-primary">${cityTag.value}</button>`);
+  cityMember.push(cityTag.value)
 }
 
 addButton.addEventListener('click',addEventListener1);
-
 button.addEventListener('click',getServerData);
 
 function getServerData() {
-
   async function load() {
     try {
-      let city_index =[],get_date=[];
+      let cityIndex =[],getDate=[];
       let totalContent={};
 
       const datas = await Promise.all(setDate().map(date => {
@@ -27,32 +25,14 @@ function getServerData() {
         }
       ));         //모든 데이터 값 읽어오기
 
-      datas[0].body__items__item__gubun.data.forEach((data) => {
-        if (cityMember.indexOf(data) !== -1) {
-          city_index.push(datas[0].body__items__item__gubun.data.indexOf(data));
-        }
-      });         //해당 지역을 가진 index 찾기
+      console.log(datas);
 
-      datas[0].body__items__item__gubun.data.forEach((data) => {
-        city_index.map(index=>{
-          if(data === datas[0].body__items__item__gubun.data[index]) {  //해당 지역을 가진 인덱스라면
-            for(let i=0;i<datas.length;i++) {
-              if(i===0)     //첫값은 배열 형태로 넣고
-                totalContent[data] = [datas[i].body__items__item__incDec.data[index]];
-              else          //배열형태이므로 나머지는 push메소드를 이용해 넣는다.
-                totalContent[data].push(datas[i].body__items__item__incDec.data[index]);
-            }
-          }
-        })
-      });
+      addCityIndex(datas,cityMember,cityIndex); //해당 지역을 가진 index 찾기
+      checkIndex(datas,cityIndex,totalContent); //해당 지역을 가진 인덱스마다의 객체의 프로퍼티인 배열에 값을 넣는다
+      addCityDate(datas,getDate);               //해당 지역을 가진 index를 통해 날짜 배열인 get_date저장
 
-      datas.forEach(data => {
-        get_date.push(data.body__items__item__stdDay.data[0]);
-      })      //해당 지역을 가진 index를 통해 날짜 배열인 get_date저장
-
-      let df_sum1 = new dfd.DataFrame(totalContent, {index: get_date});  // totalContent와 날짜인 get_date를 통해 df_sum 생성
+      const df_sum1 = new dfd.DataFrame(totalContent, {index: getDate});  // totalContent와 날짜인 get_date를 통해 df_sum 생성
       df_sum1.plot('plot').line();      //결과 출력
-
 
       // 해야할 부분 : 추가한 도시 버튼 클릭시 삭제기능 구현 , 전반적 코드 정리 필요
 
@@ -61,11 +41,42 @@ function getServerData() {
       alert('데이터가 존재하지 않거나 잘못되었습니다.');
     }
   }
-
   load();
 }
 
-function setDate(){
+const addCityIndex = (datas,cityMember,cityIndex)=>{
+  datas[0].body__items__item__gubun.data.forEach((data) => {
+    if (cityMember.indexOf(data) !== -1)
+      cityIndex.push(datas[0].body__items__item__gubun.data.indexOf(data))
+  });
+}
+
+
+const checkIndex = (datas,cityIndex,totalContent)=>{
+  datas[0].body__items__item__gubun.data.forEach((data) => {
+    cityIndex.map(index=>{
+      if(data === datas[0].body__items__item__gubun.data[index])   //해당 지역을 가진 인덱스라면
+        pushValue(totalContent,datas,data,index); //객체의 프로퍼티인 배열에 값을 추가
+    })
+  });
+}
+
+const pushValue = (totalContent,datas,data,index)=>{
+  for(let i=0;i<datas.length;i++) {
+    if(i===0)     //첫값은 배열 형태로 넣고
+      totalContent[data] = [datas[i].body__items__item__incDec.data[index]];
+    else          //배열형태이므로 나머지는 push메소드를 이용해 넣는다.
+      totalContent[data].push(datas[i].body__items__item__incDec.data[index]);
+  }
+}
+
+const addCityDate = (datas,getDate)=>{
+  datas.forEach(data => {
+    getDate.push(data.body__items__item__stdDay.data[0]);
+  })
+}
+
+const setDate = ()=>{
   let input = document.getElementById('input')
   let days = document.getElementById('days');
   let returnDay = checkDuring(days.options[days.selectedIndex].text);
@@ -82,11 +93,10 @@ function setDate(){
     Day = tDate.getDate().toString().length==1? '0'+tDate.getDate().toString() : tDate.getDate().toString();
     date_array.push(Year+Month+Day);    //합친 내용을 배열로 만들어 준다.
   }
-
   return date_array;     //해당 배열을 반환한다.
 }
 
-function checkDuring(day){
+const checkDuring = (day) => {
   const $sample = {
     '1주일' : 7,
     '2주일' :  14,
