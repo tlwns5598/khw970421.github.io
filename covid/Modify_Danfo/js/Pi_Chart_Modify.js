57219dd3f9a03b66719122753d9ce69be882f606
khw970421 <khw970421@kakao.com>
2021-03-25 15:10:14 +0900

    Pi_Chart_Modify에 다운로드 기능추가(성공)

--- covid/Modify_Danfo/js/Pi_Chart_Modify.js
+++ covid/Modify_Danfo/js/Pi_Chart_Modify.js
@@ -3,6 +3,37 @@ import {url} from './Data.js'
 let input = document.getElementById('input')
 let button = document.getElementById('but');
 
+
+function downloadCSV(location, increase){
+  let array = [];
+  array.push(location);
+  array.push(increase);
+
+  let val = "";
+
+  // jquery 사용하지 않는 경우
+    for (let j = 0; j < location.length; j++) {
+      val += array[0][j]+',';
+    }
+    val +=  "\r\n";
+    for(let k=0;k<increase.length;k++){
+      val += array[1][k]+',';
+    }
+    val +=  "\r\n";
+
+    console.log(val);
+  let downloadLink = document.createElement("a");
+  let blob = new Blob(["\ufeff"+val], { type: "text/csv;charset=utf-8" });
+  let url = URL.createObjectURL(blob);
+  downloadLink.href = url;
+  downloadLink.download = "data.csv";
+
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+
 button.addEventListener('click',function(){
   dfd.read_csv(`${url}${input.value.replace(/-/gi,'').slice(2,8)}.csv`)
     .then(
@@ -11,11 +42,16 @@ button.addEventListener('click',function(){
         const gubun_Length_Except_Sum = data.body__items__item__gubun.data.length-1;
 
         let df = new dfd.DataFrame({
-          Price: data.body__items__item__incDec.data.slice(0,incDec_Length_Except_Sum),   //표의 맨 아래 합계를 제거한 내용들
+          Increase: data.body__items__item__incDec.data.slice(0,incDec_Length_Except_Sum),   //표의 맨 아래 합계를 제거한 내용들
           Location : data.body__items__item__gubun.data.slice(0,gubun_Length_Except_Sum),
           Type: data.body__items__item__gubun.data.slice(0,gubun_Length_Except_Sum)
         })
-        df.plot("plot_div").pie({ values: "Price", labels: "Type" })
+
+        const location = df.Location.data;
+        const Increase = df.Increase.data;
+        document.querySelector('.but').addEventListener('click',()=>downloadCSV(location,Increase));
+
+        df.plot("plot_div").pie({ values: "Increase", labels: "Type" })
       }
     )
     .catch(() => {
